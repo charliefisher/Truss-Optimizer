aa6ef5548b43fbafd9ac139c2edbe471e83c9804
Charlie Fisher <charliefisherhome@gmail.com>
2019-03-20 22:42:39 -0400

    Finished general system solver and truss class declaration

--- Truss.hpp
+++ Truss.hpp
@@ -1,15 +1,35 @@
-/*
- * Truss.hpp
- *
- *  Created on: Mar. 20, 2019
- *      Author: charl
- */
 
 #ifndef TRUSS_HPP_
 #define TRUSS_HPP_
+#endif
 
+class Truss {
+	public:
+		const unsigned int MAX_MEMBER_FORCE = 7;
 
+		// assume that up and right are positive for position
+		// assume that up and right are positive for forces
+		struct Joint{
+			Joint();
+			double x, y;
+			bool fixedX, fixedY;
+			double externalX, externalY;
+		};
 
+		Truss(unsigned int numJoints);
+		~Truss();
+		Joint* getJoints();
+		void solve();
 
+	private:
+		unsigned int numJoints;
+		Joint* joints;
+		Joint *pin, *normalJoint;
 
-#endif /* TRUSS_HPP_ */
+		// solves for moment above pin and net x and y force
+		void solveGeneralSystem();
+		void solveJoint(Joint* j);
+
+		// should not be called
+		Truss();
+};
